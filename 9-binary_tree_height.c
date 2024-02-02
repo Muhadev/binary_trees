5897b7f28bf5a799361729814721b27436485369
Muhadev <muhammedfayemi@gmail.com>
2024-02-01 17:11:33 -0800

    9-binary_tree_height.c added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
 		return (0);
-	left_height = binary_tree_height(tree->left) : 0;
-	right_height = binary_tree_height(tree->right) : 0;
-	return (left_height > right_height ? left_height : right_height);
+
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	
+	return ((l > r) ? l : r);
 }
