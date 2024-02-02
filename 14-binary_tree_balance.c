823016cdeafe706a1a15dd680203b10df91426a5
Muhadev <muhammedfayemi@gmail.com>
2024-02-02 14:56:16 -0800

    14-binary_tree_balance.c added

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_height_c - Measures the height of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the height.
  *
  * Return: Height of the binary tree. If tree is NULL, return 0.
