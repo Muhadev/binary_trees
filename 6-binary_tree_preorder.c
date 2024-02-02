575bcae5112a6cf0970e0b541364c4406494aa18
Muhadev <muhammedfayemi@gmail.com>
2024-02-01 16:27:20 -0800

    6-binary_tree_preorder.c added

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,7 +4,8 @@
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node.
  *
- * Description: The value in the node must be passed as a parameter to the function.
+ * Description: The value in the node must be
+ * passed as a parameter to the function.
  * If tree or func is NULL, do nothing.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
