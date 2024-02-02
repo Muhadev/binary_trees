5d88dd975496f063c85516a157192b13328b877f
Muhadev <muhammedfayemi@gmail.com>
2024-02-01 16:37:10 -0800

    7-binary_tree_inorder.c added

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,14 +4,15 @@
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node.
  *
- * Description: The value in the node must be passed as a parameter to the function.
+ * Description: The value in the node
+ * must be passed as a parameter to the function.
  * If tree or func is NULL, do nothing.
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	 if (tree == NULL || func == NULL)
-		 return;
-	  binary_tree_inorder(tree->left, func);
-	  func(tree->n);
-	  binary_tree_inorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
